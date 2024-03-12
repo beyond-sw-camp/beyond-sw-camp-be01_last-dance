4e061f5dcf87d56d7bfba81378c57568af261997
dmario24 <becky2sawyer@gmail.com>
2024-03-12 17:13:35 +0900

    fix stop.sh

--- stop.sh
+++ stop.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
-if [ -f pid ]; then
-    echo 'stop service ...'
-    kill $(cat pid)
+TIME_NOW=$(date +%c)
+
+# 현재 구동 중인 애플리케이션 pid 확인
+CURRENT_PID=$(pgrep -f http.server)
+
+# 프로세스가 켜져 있으면 종료
+if [ -z $CURRENT_PID ]; then
+  echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
 else
-    echo 'pid file does not exist.'
-    kill $(pgrep -f http.server)
+  echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 " >> $DEPLOY_LOG
+  kill -15 $CURRENT_PID
 fi
