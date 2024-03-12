3d56358c6580077ba534b471cec546dbc7a29758
dmario24 <becky2sawyer@gmail.com>
2024-03-12 17:22:56 +0900

    log path

--- stop.sh
+++ stop.sh
@@ -7,8 +7,8 @@ CURRENT_PID=$(pgrep -f http.server)
 
 # 프로세스가 켜져 있으면 종료
 if [ -z $CURRENT_PID ]; then
-  echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
+  echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> deploy.log
 else
-  echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 " >> $DEPLOY_LOG
+  echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 " >> deploy.log
   kill -15 $CURRENT_PID
 fi
